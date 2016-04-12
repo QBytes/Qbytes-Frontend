4bcd7143fd76bb1e2ab8c6c80e234cfe15085382
adityasan92 <adityasan92@gmail.com>
2016-04-11 21:31:36 -0400

    some css changes to the navbar

--- src/js/containers/header.container.js
+++ src/js/containers/header.container.js
@@ -3,22 +3,29 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { SignOutButton } from "redux-auth";
 
+const style = {
+  paddingLeft: 50,
+  fontWeight:'bold'
+};
+
 class HeaderContainer extends Component {
 
   render(){
     return (
-      <nav>
+      <nav className="blue-grey lighten-5">
         <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">Q-Byte</Link>
+          <Link to="/" className="brand-logo red-text accent-3 text-darken-3" style={{...style}}>Q-Bytes</Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/signUp">Sign Up</Link></li>
-            <li><Link to="/signIn">Sign In</Link></li>
-            <SignOutButton />
+
+            <li ><Link to="/signUp"><button className="waves-effect waves-light  red accent-3 btn">Sign Up</button></Link></li>
+            <li><Link to="/signIn"><button className="waves-effect waves-light red accent-3 btn">Log In</button></Link></li>
+            {  /*<SignOutButton />*/}
           </ul>
         </div>
     </nav>
     );
   }
+
 }
 
 function mapStateToProps (state) {
